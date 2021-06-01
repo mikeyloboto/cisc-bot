09994c33f27bd0a1998ce94a5e0e15e70cee5f51
Evilbas <dimonloboto@gmail.com>
2021-06-01 19:12:23 -0400

    Temporarily removed disc4j functionality (tomcat)

--- src/main/java/com/evilbas/discgm/DiscordConfig.java
+++ src/main/java/com/evilbas/discgm/DiscordConfig.java
@@ -14,12 +14,7 @@ import org.springframework.core.env.Environment;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.object.entity.User;
-
-import com.evilbas.discgm.discord.domain.MessageCommand;
-import com.evilbas.discgm.util.Constants;
 
 @Configuration
 public class DiscordConfig {
@@ -28,31 +23,19 @@ public class DiscordConfig {
     @Autowired
     private Environment env;
 
-    @Autowired
-    private CommandHandler commandHandler;
+    // @Bean
+    // public GatewayDiscordClient discordClient() {
+    // log.debug("init discord client");
+    // GatewayDiscordClient client =
+    // DiscordClientBuilder.create(env.getProperty("disc.token")).build().login()
+    // .block();
+    // client.getEventDispatcher().on(ReadyEvent.class).subscribe(event -> {
+    // final User self = event.getSelf();
+    // System.out.println(String.format("Logged in as %s#%s", self.getUsername(),
+    // self.getDiscriminator()));
+    // });
 
-    @Bean
-    public GatewayDiscordClient discordClient() {
-        log.debug("init discord client");
-        GatewayDiscordClient client = DiscordClientBuilder.create(env.getProperty("disc.token")).build().login()
-                .block();
-        client.getEventDispatcher().on(ReadyEvent.class).subscribe(event -> {
-            final User self = event.getSelf();
-            System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
-        });
-
-        client.getEventDispatcher().on(MessageCreateEvent.class).subscribe(event -> {
-            final String content = event.getMessage().getContent();
-
-            for (final Map.Entry<String, MessageCommand> entry : commandHandler.getCommands().entrySet()) {
-                if (content.startsWith(Constants.DISC_BOT_PREFIX + entry.getKey())) {
-                    entry.getValue().execute(event);
-                    break;
-                }
-            }
-        });
-
-        client.onDisconnect().block();
-        return client;
-    }
+    // client.onDisconnect().block();
+    // return client;
+    // }
 }
--- src/main/java/com/evilbas/discgm/discord/CommandHandler.java
+++ src/main/java/com/evilbas/discgm/discord/CommandHandler.java
@@ -5,34 +5,61 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.Channel;
 
 import com.evilbas.discgm.discord.domain.MessageCommand;
+import com.evilbas.discgm.util.Constants;
 
 @Component
 public class CommandHandler {
 
 	private Map<String, MessageCommand> commands;
 
-	@PostConstruct
-	public void init() {
-		commands = new HashMap<String, MessageCommand>();
-		commands.put("ping", event -> event.getMessage().getChannel().block().createMessage("Pong!").block());
-		commands.put("status", event -> {
-			if (event.getMessage().getChannel().block().getType().equals(Channel.Type.DM)) {
-				Message messageRef = event.getMessage().getChannel().block().createMessage("Checking").block();
-				messageRef.pin().block();
-			}
-		});
-		commands.put("me", event -> {
-			String sendingUser = event.getMessage().getAuthor().get().getUsername() + "#" + event.getMessage().getAuthor().get().getDiscriminator();
-			event.getMessage().getChannel().block().createMessage("<@" + event.getMessage().getAuthor().get().getId().asLong() + ">").block();
-		});
+	// @Autowired
+	// GatewayDiscordClient discordClient;
 
-	}
+	// @PostConstruct
+	// public void init() {
+
+	// discordClient.getEventDispatcher().on(MessageCreateEvent.class).subscribe(event
+	// -> {
+	// final String content = event.getMessage().getContent();
+
+	// for (final Map.Entry<String, MessageCommand> entry :
+	// getCommands().entrySet()) {
+	// if (content.startsWith(Constants.DISC_BOT_PREFIX + entry.getKey())) {
+	// entry.getValue().execute(event);
+	// break;
+	// }
+	// }
+	// });
+
+	// commands = new HashMap<String, MessageCommand>();
+	// commands.put("ping", event ->
+	// event.getMessage().getChannel().block().createMessage("Pong!").block());
+	// commands.put("status", event -> {
+	// if
+	// (event.getMessage().getChannel().block().getType().equals(Channel.Type.DM)) {
+	// Message messageRef =
+	// event.getMessage().getChannel().block().createMessage("Checking").block();
+	// messageRef.pin().block();
+	// }
+	// });
+	// commands.put("me", event -> {
+	// String sendingUser = event.getMessage().getAuthor().get().getUsername() + "#"
+	// + event.getMessage().getAuthor().get().getDiscriminator();
+	// event.getMessage().getChannel().block()
+	// .createMessage("<@" + event.getMessage().getAuthor().get().getId().asLong() +
+	// ">").block();
+	// });
+
+	// }
 
 	public Map<String, MessageCommand> getCommands() {
 		return commands;
