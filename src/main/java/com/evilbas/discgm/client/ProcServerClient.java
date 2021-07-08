4099625106959c91f1e24055efc39c07f847eca2
Evilbas <dimonloboto@gmail.com>
2021-07-08 09:39:06 -0400

    Moving endpoints to post

--- src/main/java/com/evilbas/discgm/client/ProcServerClient.java
+++ src/main/java/com/evilbas/discgm/client/ProcServerClient.java
@@ -2,7 +2,9 @@ package com.evilbas.discgm.client;
 
 import com.evilbas.rslengine.creature.Encounter;
 import com.evilbas.rslengine.networking.CombatResultWrapper;
+import com.evilbas.rslengine.networking.InventoryActionRequest;
 import com.evilbas.rslengine.networking.InventoryInteractionWrapper;
+import com.evilbas.rslengine.networking.ResultWrapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -68,15 +70,18 @@ public class ProcServerClient {
     public InventoryInteractionWrapper listInventory(String characterGuid) {
         log.info("List inventory request");
         HttpHeaders headers = new HttpHeaders();
+
+        InventoryActionRequest request = new InventoryActionRequest();
+        request.setGuid(characterGuid);
+
         headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serverUri + "/inventory").queryParam("guid",
-                characterGuid);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serverUri + "/inventory");
 
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         ResponseEntity<InventoryInteractionWrapper> response = restTemplate.exchange(builder.toUriString(),
-                HttpMethod.GET, entity, InventoryInteractionWrapper.class);
+                HttpMethod.POST, entity, InventoryInteractionWrapper.class, request);
         if (response.getStatusCode() == HttpStatus.OK) {
             return response.getBody();
         }
@@ -88,17 +93,40 @@ public class ProcServerClient {
         HttpHeaders headers = new HttpHeaders();
         headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serverUri + "/inventory/use")
-                .queryParam("guid", characterGuid).queryParam("item", item);
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serverUri + "/inventory/use");
+
+        InventoryActionRequest request = new InventoryActionRequest();
+        request.setGuid(characterGuid);
+        request.setItem(item);
 
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         ResponseEntity<InventoryInteractionWrapper> response = restTemplate.exchange(builder.build().toUriString(),
-                HttpMethod.GET, entity, InventoryInteractionWrapper.class);
+                HttpMethod.POST, entity, InventoryInteractionWrapper.class, request);
         log.info("Resp: {}", response);
         log.info("Req: {}", builder.toUriString());
         if (response.getStatusCode() == HttpStatus.OK) {
             log.info("Body: {}", response.getBody().getInventory());
+            return response.getBody();
+        }
+        return null;
+    }
+
+    private ResultWrapper sendToServer(String endpoint, Object requestBody, HttpMethod method, Class cl) {
+        log.info("use item request");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serverUri + endpoint);
+
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<ResultWrapper> response = restTemplate.exchange(builder.build().toUriString(), method, entity,
+                ResultWrapper.class, requestBody);
+        log.debug("Resp: {}", response);
+        log.debug("Req: {}", builder.toUriString());
+        if (response.getStatusCode() == HttpStatus.OK) {
+            log.debug("Body: {}", response.getBody());
             return response.getBody();
         }
         return null;
