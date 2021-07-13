4662c934fbf47cd388b1f1146b96b5c2bb90c88c
Evilbas <dimonloboto@gmail.com>
2021-07-13 19:06:12 -0400

    metrics

--- src/main/java/com/evilbas/discgm/ClientConfig.java
+++ src/main/java/com/evilbas/discgm/ClientConfig.java
@@ -6,14 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@PropertySources({ @PropertySource("classpath:application.properties"),
-		@PropertySource("file:${APPLICATION_EXT_PROPERTIES}/discgm-ws/env.properties") })
+@PropertySource("classpath:application.properties")
+@PropertySource("file:${APPLICATION_EXT_PROPERTIES}/discgm-ws/env.properties")
 @MapperScan("com.evilbas.discgm.dao.sql")
 @ComponentScan(basePackages = "com.evilbas.discgm")
+@ComponentScan("com.evilbas.metrics")
 @EnableAsync
 public class ClientConfig {
 
